45b4ad9f33be8f68f477914d2040590e6bde2474
bert <bertcommeine@gmail.com>
2020-03-04 14:56:51 +0100

    chromozon

--- brunozon.js
+++ brunozon.js
@@ -1,58 +1,56 @@
-function walk(rootNode){
-    // Find all the text nodes in rootNode
-    var walker = document.createTreeWalker(
-        rootNode,
-        NodeFilter.SHOW_TEXT,
-        null,
-        false
-    ),
-    node;
+walk(document.body);
+switchImages();
 
-    // Modify each text node's value
-    while (node = walker.nextNode()) {
-      handleText(node);
-    }
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+	
+	var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+	if (tagName == 'input' || tagName == 'textarea') {
+		return;
+	}
+	if (node.classList && node.classList.contains('ace_editor')) {
+		return;
+	}
 
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
 }
 
-function handleText(textNode){
-    var v = textNode.wholeText;
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
 
-    v = v.replace(/\bAmazon\b/g, "Brunozon");
-    v = v.replace(/\bamazon\b/g, "brunozon");
-    textNode.nodeValue = v;
-
+	v = v.replace(/\bAmazon\b/g, "Brunozon");
+	v = v.replace(/\bamazon\b/g, "brunozon");
+	
+	textNode.nodeValue = v;
 }
 
-// The callback used for the document body and title observers
-function observerCallback(mutations) {
-
-    var i;
-
-    mutations.forEach(function(mutation) {
-        for (i = 0; i < mutation.addedNodes.length; i++) {
-            if (mutation.addedNodes[i].nodeType === 3) {
-                // Replace the text for text nodes
-                handleText(mutation.addedNodes[i]);
-            } else if (mutation.addedNodes[i].nodeType === 1) {
-                // Replace the text for text nodes
-                handleText(mutation.addedNodes[i]);
-                walk(mutation.addedNodes[i]);
-            } else {
-                // Otherwise, find text nodes within the given node and replace text
-                walk(mutation.addedNodes[i]);
-            }
-        }
-    });
-}
-
-// Walk the doc (document) body, replace the title, and observe the body and title
-function walkAndObserve(doc) {
-
+function switchImages() {
     let logos = document.querySelectorAll(".a-icon");
     for (let l of logos) {
         if (! (l.classList.contains("a-icon-next-rounded") || l.classList.contains('a-icon-previous-rounded'))) {
-            l.style.backgroundImage = 'url("' + browser.extension.getURL("icons/brunozon-logo.png") + '")';
+//            l.style.backgroundImage = 'url("' + browser.extension.getURL("icons/brunozon-logo.png") + '")';
             l.style.backgroundSize =  'cover';
             l.style.backgroundPosition =  '0px 0px';
             l.style.height =  '38px';
@@ -61,33 +59,10 @@ function walkAndObserve(doc) {
 
     logos = document.querySelectorAll(".nav-logo-base");
     for (let l of logos) {
-        l.style.backgroundImage = 'url("' + browser.extension.getURL("icons/brunozon-logo-inverted.png") + '")';
+        l.style.backgroundImage = 'url("' + chrome.extension.getURL("icons/brunozon-logo-inverted.png") + '")';
         l.style.backgroundSize =  'cover';
         l.style.backgroundPosition =  '0px 0px';
         l.style.height =  '38px';
         l.style.width =  '100px';
     }
-
-    var docTitle = doc.getElementsByTagName('title')[0],
-    observerConfig = {
-        characterData: true,
-        childList: true,
-        subtree: true
-    },
-    bodyObserver, titleObserver;
-
-    // Do the initial text replacements in the document body and title
-    walk(doc.body);
-    doc.title = replaceText(doc.title);
-
-    // Observe the body so that we replace text in any added/modified nodes
-    bodyObserver = new MutationObserver(observerCallback);
-    bodyObserver.observe(doc.body, observerConfig);
-
-    // Observe the title so we can handle any modifications there
-    if (docTitle) {
-        titleObserver = new MutationObserver(observerCallback);
-        titleObserver.observe(docTitle, observerConfig);
-    }
 }
-walkAndObserve(document);
